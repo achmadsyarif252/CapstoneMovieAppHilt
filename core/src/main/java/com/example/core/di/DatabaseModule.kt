fdec23d135ffc0e851baccd752fb7d135bca1770
achmad2002 <19kaizen@gmail.com>
2023-03-22 17:45:30 +0700

    fix : add memory leak canary and encrypt database

--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -9,18 +9,24 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
+    val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+    val factory = SupportFactory(passphrase)
 
     @Singleton
     @Provides
     fun provideDatabase(@ApplicationContext context: Context): MovieDatabase = Room.databaseBuilder(
         context,
         MovieDatabase::class.java, "Movie.db"
-    ).fallbackToDestructiveMigration().build()
+    ).fallbackToDestructiveMigration()
+        .openHelperFactory(factory)
+        .build()
 
     @Provides
     fun provideMovieDao(database: MovieDatabase): MovieDao = database.movieDao()
