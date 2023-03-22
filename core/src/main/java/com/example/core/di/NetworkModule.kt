1d487f88007d38da5e14ab0a25c6c1fe39cb6f0f
achmad2002 <19kaizen@gmail.com>
2023-03-22 21:30:36 +0700

    fix : add ssl certificate pinning

--- core/src/main/java/com/example/core/di/NetworkModule.kt
+++ core/src/main/java/com/example/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -14,6 +15,10 @@ import java.util.concurrent.TimeUnit
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
+    val hostname = "api.themoviedb.org"
+    val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/NPIMWkzcNG/MyZsVExrC6tdy5LTZzeeKg2UlnGG55UY=")
+        .build()
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
@@ -21,6 +26,7 @@ class NetworkModule {
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
